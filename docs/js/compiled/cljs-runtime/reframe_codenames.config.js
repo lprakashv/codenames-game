a0eab79f0a3164c9f51b2c3684ecf024b1b33ff5
Lalit Vatsal <lprakashv@gmail.com>
2021-02-11 16:10:53 +0530

    fixed git

--- docs/js/compiled/cljs-runtime/reframe_codenames.config.js
+++
@@ -1,4 +0,0 @@
-goog.provide('reframe_codenames.config');
-reframe_codenames.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=reframe_codenames.config.js.map
--- docs/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { reframe_codenames.core.init(); } catch (e) { console.error("An error occurred when calling (reframe-codenames.core/init)"); throw(e); }
